b58f7e408dfe8924cfdfc18ef70438f4789049d4
charishma7699 <103839286+Chundu2022@users.noreply.github.com>
2024-01-16 11:53:28 -0600

    Static

--- Static.java
+++ Static.java
@@ -1,17 +1,15 @@
 public class Static {
-    // Static variable
+
     static int Variable = 60;
 
-    // Static method that returns an int
     public static int StaticVariable() {
         return Variable;
     }
 
     public static void main(String[] args) {
-        // Access the static variable
+
         System.out.println(Static.Variable);
 
-        // Access the static variable using the static method
         System.out.println(Static.StaticVariable());
     }
 }
