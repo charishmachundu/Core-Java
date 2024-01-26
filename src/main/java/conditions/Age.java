0b88848bb69dc06e957363e8299c51699dbd7195
charishmachundu <charishmachundu@gmail.com>
2024-01-25 22:38:32 -0600

    Programs

---
+++ src/main/java/conditions/Age.java
@@ -0,0 +1,17 @@
+package conditions;
+
+public class Age {
+    public static void main(String[] args){
+        int age =15;
+
+        if(age <= 13){
+            System.out.println("Child");
+        }
+       else if(age>=13 && age<=19){
+            System.out.println("Teen");
+        }
+        else {
+            System.out.println("Adult");
+        }
+    }
+}
